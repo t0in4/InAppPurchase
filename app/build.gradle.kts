87c4828902b956d9d79d44c9fa96b673128fdc51
t0in4 <t0in4@yahoo.com>
2024-03-05 22:14:09 +0900

    added libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.authentification.inapppurchase"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.authentification.inapppurchase"
@@ -44,6 +44,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // adding libraries - start
+    implementation("com.android.billingclient:billing:6.1.0")
+    implementation("com.android:billingclient:billing-ktx:6.1.0")
+    implementation("com.google.guava:guava:24.1-jre")
+    implementation("com.google.guava:guava:27.0.1-android")
+    // adding libraries - end
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
